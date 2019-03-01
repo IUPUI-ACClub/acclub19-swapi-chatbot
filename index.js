912f39af3e488c3cca48d68ec1ae0b01072d2509
ajroembk <ajroembk@iupui.edu>
2019-03-01 15:37:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// STARTER CODE FOR A CLOUD FUNCTION THAT IS THE FULFILLMENT FOR AN ACTION ON GOOGLE
+
+'use strict';
+
+const {dialogflow} = require('actions-on-google');
+
+const functions = require('firebase-functions');
+
+const swapi = require('swapi-node');
+
+
+
+const app = dialogflow({debug:true});
+
+// const swapi = require('swapi-node');
+
+// SAMPLE INTENT HANDLER
+ app.intent("SAMPLE_INTENT_NAME_FROM_DIALOGFLOW", (conv) => {
+
+  // LOGIC FOR THIS INTENT GOES HERE:
+
+    let Luke = Luke.getPerson(1);
+    conv.ask(Luke.name)
+    swapi.getPerson(1).then((result) => {
+    return result;
+});
+
+  // TO RETURN TO DIALOGFLOW AND CONTINUE THE CONVERSATION, USE conv.ask()
+    // conv.ask(`Let's chat some more.`);
+
+  // TO RETURN TO DIALOGFLOW AND END THE CONVERSATION, USE conv.close()
+    // conv.close(`Goodbye.`);
+ });
+
+exports.generateStarWarsUniverse = functions.https.onRequest(app);
